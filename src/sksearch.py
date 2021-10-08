6862af30a0021e9018fab6164c437d38a8b94566
Jerrad Genson <contact@jerradgenson.com>
2021-10-09 01:59:29 +0200

    Add pso and corresponding unit tests

--- src/sksearch.py
+++ src/sksearch.py
@@ -14,7 +14,8 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 """
 
 import enum
-import random
+
+import numpy as np
 
 
 class HillClimbingMethods(enum.Enum):
@@ -35,24 +36,23 @@ class HillClimbingMethods(enum.Enum):
     STOCHASTIC = enum.auto()
 
 
-def hill_climbing(initial_state,
-                  score,
+def hill_climbing(gen_initial_state,
+                  loss,
                   get_successor,
                   max_iterations=1000,
                   method=HillClimbingMethods.STEEPEST_ASCENT,
                   restarts=0,
-                  gen_initial_state=None,
                   initial_momentum=0,
                   max_momentum=3):
     """
     Find a solution to a search problem using hill climbing.
 
     Args:
-      initial_state: the initial state that the search space is in. May
-                     be an arbitrary object.
-      score: A function that accepts search space states and returns a
-             score between 0 and 1 (inclusive), where higher scores are
-             better than lower scores.
+      gen_initial_state: A function that takes no arguments and returns
+                         a new search space state.
+      loss: A function that accepts search space states and returns a
+             score >= 0 and 1 where lower scores are superior to higher
+             to scores.
       get_successor: A generator function that accepts a search space
                      state and yields the next state.
       max_iterations: Maximum iterations to perform hill climbing, not
@@ -61,9 +61,6 @@ def hill_climbing(initial_state,
               `HillClimbingMethods`.
       restarts: The number of times to restart hill climbing from an
                 initial state.
-      gen_initial_state: A function that takes no arguments and returns
-                         a new search space state. If left to None, then
-                         `initial_state` will be reused on every restart.
       initial_momentum: The amount of momentum to begin with for overcoming
                         local optima. If left to 0, momentum will not be
                         utilized.
@@ -75,9 +72,11 @@ def hill_climbing(initial_state,
 
     """
 
+    initial_state = gen_initial_state()
+
     # Global best is the best state found so far, including restarts.
     global_best_state = initial_state
-    global_best_score = score(initial_state)
+    global_best_score = loss(initial_state)
 
     # Local best is the best state found so far, not including restarts.
     local_best_state = global_best_state
@@ -96,11 +95,12 @@ def hill_climbing(initial_state,
                 random.shuffle(successors)
 
             for successor in successors:
-                successor_score = score(successor)
+                successor_score = loss(successor)
                 if successor_score > best_successor_score:
                     best_successor = successor
                     best_successor_score = successor_score
-                    if method in (HillClimbingMethods.SIMPLE, HillClimbingMethods.STOCHASTIC):
+                    if method in (HillClimbingMethods.SIMPLE,
+                                  HillClimbingMethods.STOCHASTIC):
                         break
 
             slope = best_successor_score / local_best_score
@@ -125,7 +125,55 @@ def hill_climbing(initial_state,
             break
 
         restarts = restarts - 1
-        local_best_state = gen_initial_state() if gen_initial_state else initial_state
-        local_best_score = score(local_best_state)
+        local_best_state = gen_initial_state()
+        local_best_score = loss(local_best_state)
 
     return global_best_state, global_best_score
+
+
+def pso(solutions, loss,
+        c1=2,
+        c2=2,
+        vmax=2,
+        min_error=0,
+        max_iter=1000,
+        rng=None):
+    if not rng:
+        rng = np.random.default_rng()
+
+    solutions = list(solutions)
+    pbest = solutions
+    pbest_fitness = list(np.full(len(solutions), np.inf))
+    velocity = list(np.full((len(solutions),) + solutions[0].shape, 0))
+    for _ in range(max_iter):
+        gbest = None
+        gbest_fitness = np.inf
+        for i, solution in enumerate(solutions):
+            fitness = loss(solution)
+            if fitness <= min_error:
+                return solution, fitness
+
+            if fitness < pbest_fitness[i]:
+                pbest_fitness[i] = fitness
+                pbest[i] = solution
+
+            if gbest is None or fitness < gbest_fitness:
+                gbest = solution
+                gbest_fitness = fitness
+
+        for i, solution in enumerate(solutions):
+            velocity[i] = (velocity[i]
+                           + c1
+                           * rng.random()
+                           * (pbest[i] - solution)
+                           + c2
+                           * rng.random()
+                           * (gbest - solution))
+
+            velocity[i] = np.where(np.abs(velocity[i]) > vmax,
+                                   vmax * np.sign(velocity[i]),
+                                   velocity[i])
+
+        solutions = [s + velocity[i] for i, s in enumerate(solutions)]
+
+    return gbest, gbest_fitness
---
+++ src/tests/unit/test_sksearch.py
@@ -0,0 +1,94 @@
+"""
+Unit tests for sksearch.py
+
+Copyright 2021 Jerrad M. Genson
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+"""
+
+import unittest
+
+import numpy as np
+
+import sksearch
+
+
+def ordering_cost(a):
+    swaps = 0
+    a0 = [a[0]]
+    unsorted = True
+    while unsorted:
+        unsorted = False
+        for x in a[1:]:
+            if a0[-1] > x:
+                unsorted = True
+                swaps = swaps + 1
+                a0.append(a0[-1])
+                a0[-2] = x
+
+            else:
+                a0.append(x)
+
+        a = a0
+        a0 = [a[0]]
+
+    return swaps
+
+
+def random_solutions(a, n,
+                     rng=np.random.default_rng(),
+                     shuffle=True,
+                     mutation=2):
+    solutions = []
+    for _ in range(n):
+        if shuffle:
+            a0 = rng.choice(a, size=len(a), replace=False)
+            if mutation:
+                a0 = mutate(a0, mutation, rng)
+
+        elif mutation:
+            a0 = mutate(a, mutation, rng)
+
+        else:
+            raise ValueError('shuffle can not be False while mutation is 0')
+
+        solutions.append(a0)
+
+    return solutions
+
+
+def mutate(a, magnitude=2, rng=np.random.default_rng()):
+    mutator = rng.random(a.shape) * rng.choice([1, -1], a.shape) * magnitude
+    return a * mutator
+
+
+def square_root2(x):
+    sign_penalty = 1 if x >= 0 else 2
+    return np.abs(x * x - 2) * sign_penalty
+
+
+class TestPSO(unittest.TestCase):
+    def test_square_root2(self):
+        rng = np.random.default_rng(0)
+        guess = np.full(1, 2 * rng.random())
+        solutions = random_solutions(guess, 50,
+                                     rng=rng,
+                                     shuffle=False,
+                                     mutation=2)
+
+        best, distance = sksearch.pso(solutions, square_root2,
+                                      vmax=0.5,
+                                      max_iter=1000,
+                                      rng=rng,
+                                      c1=1,
+                                      c2=1)
+
+        self.assertAlmostEqual(best[0], 1.4109018179007498)
+        self.assertAlmostEqual(square_root2(best)[0], distance[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
